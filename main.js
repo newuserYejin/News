efbcfacc15bb0db6195c097216abfba51c38123b
jin <jin200126@gmail.com>
2024-02-20 01:06:09 +0900

    third commit(show news)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_KEY = 'feaeef5a51ef41c49fceb7a0ca1046bb'
-let news = []
+let newsList = []
 
 const getLatesNews = async () => {
     const url = new URL(
@@ -8,8 +8,29 @@ const getLatesNews = async () => {
     )
     const response = await fetch(url)
     const data = await response.json()
-    news = data.articles
-    console.log("data: ", news)
+    newsList = data.articles
+    console.log("data: ", newsList)
+    render()
+}
+
+const render = () => {
+    const newsHTML = newsList.map((news) => {
+        return `
+        <div class="news row">
+            <div class="col-lg-4">
+                <img class="news_img"
+                    src=${news.urlToImage}>
+            </div>
+            <div class="col-lg-8">
+                <h2>${news.title}</h2>
+                <p>${news.description}</p>
+                <div>${news.source.name} ${news.publishedAt}</div>
+            </div>
+        </div>
+        `
+    }).join("")
+
+    document.getElementById('news_board').innerHTML = newsHTML
 }
 
 getLatesNews()          // 함수 부르는거 까먹지 말기
