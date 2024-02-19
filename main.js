6f8306303b7ad71ef6283b14208cf349854d9994
jin <jin200126@gmail.com>
2024-02-20 00:07:22 +0900

    first news commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const API_KEY = 'feaeef5a51ef41c49fceb7a0ca1046bb'
+let news = []
+
+const getLatesNews = async () => {
+    const url = new URL(
+        // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        'http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines'
+    )
+    const response = await fetch(url)
+    const data = await response.json()
+    news = data.articles
+    console.log("data: ", news)
+}
+
+getLatesNews()          // 함수 부르는거 까먹지 말기
+
