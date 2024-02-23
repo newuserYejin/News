a15f10551d343f334457e9ca288a0888d56418f9
jin <jin200126@gmail.com>
2024-02-23 23:33:15 +0900

    13 video

--- mymain.js
+++ mymain.js
@@ -14,46 +14,32 @@ const getNews = async () => {
         const response = await fetch(url)
         const data = await response.json()
         if (response.status === 200) {
-            if (data.articles.length === 0) {
-                throw new Error("No result for this search")
-            }
             newsList = data.articles
-            render()
+            // return newsList
+            render(newsList)
         } else {
             throw new Error(data.message)
         }
     } catch (error) {
-        console.log("error: ", error.message)
+        console.log("error: ", error)
+        console.log(error.message)
         errorRender(error.message)
     }
 }
 
 const getLatesNews = async () => {
-    url = new URL(
-        'https://myfirstnewspage.netlify.app/top-headlines'
-    )
-    getNews()
+    url = new URL('https://myfirstnewspage.netlify.app/top-headlines')
+
+    await getNews()
+
+    console.log("data: ", newsList)
 }
 
-const getNewsByCategory = async (event) => {
-    const category = event.target.textContent.toLowerCase()
-    url = new URL(`https://myfirstnewspage.netlify.app/top-headlines?category=${category}`)
-
-    getNews()
-}
-
-const getNewsByKeyword = async () => {
-    const keyword = userInput.value
-    console.log(keyword)
-
-    url = new URL(`https://myfirstnewspage.netlify.app/top-headlines?q=${keyword}`)
-
-    getNews()
-}
-
-const render = () => {
-    const newsHTML = newsList.map((news) => {
-        return `
+const render = (e) => {
+    let newsHTML
+    if (e) {
+        newsHTML = newsList.map((news) => {
+            return `
         <div class="news row">
             <div class="col-lg-4">
                 <img class="news_img"
@@ -66,7 +52,15 @@ const render = () => {
             </div>
         </div>
         `
-    }).join("")
+        }).join("")
+    } else {                            // 검색 결과 없을때의 html
+        newsHTML = `
+        <div class="alert alert-danger">
+            <div class="none_news">No matches for your search</div>
+        </div>
+        `
+        console.log("결과 없음")
+    }
 
     document.getElementById('news_board').innerHTML = newsHTML
 }
@@ -93,6 +87,33 @@ side_closeButton.addEventListener("click", () => {
     side_menuArea.classList.toggle("show-menu");
 })
 
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase()
+    console.log(category)
+    url = new URL(`https://myfirstnewspage.netlify.app/top-headlines?category=${category}`)
+
+    await getNews()
+
+    render(newsList)
+}
+
+const getNewsByKeyword = async () => {
+    const keyword = userInput.value
+    console.log(keyword)
+
+    url = new URL(`https://myfirstnewspage.netlify.app/top-headlines?q=${keyword}`)
+
+    await getNews()
+
+    console.log("length:", newsList.articles)
+
+    if (newsList.length === 0) {
+        render()
+    } else {
+        render(newsList)
+    }
+}
+
 userInput.addEventListener("keypress", function (event) {       // enter 키 이벤트 추가
     if (event.key === "Enter") {
         event.preventDefault();
@@ -107,13 +128,16 @@ function show_input() {
 }
 
 const errorRender = (errorMessage) => {
+    console.log("야호:", errorMessage)
     const errorHTML = `
     <div class="alert alert-danger">
-        <div class="none_news">${errorMessage}</div>
+        <div>${errorMessage}</div>
     </div>
     `
+    console.log("호호호호호호:", errorMessage)
+    console.log("흠흠: ", errorHTML)
 
-    document.getElementById("news_board").innerHTML = errorHTML
+    document.querySelector("#news_board").innerHTML = errorHTML
 }
 
 getLatesNews()          // 함수 부르는거 까먹지 말기
